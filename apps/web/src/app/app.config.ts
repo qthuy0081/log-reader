86019be1166a830ce640425e777fb8c06465b085
Eddie <qthuy.0081@gmail.com>
2025-07-11 13:51:33 +0700

    Add tailwind css

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,15 +1,14 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };
